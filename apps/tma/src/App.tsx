2ab6f31fe4aa78665602396c1406dc3badd477ee
yeeetai <helloeric01@gmail.com>
2024-03-10 02:37:35 +0800

    feat: build url and token via urlhelper

--- apps/tma/src/App.tsx
+++ apps/tma/src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { Counter } from "./components/Counter";
-import { Jetton } from "./components/Jetton";
 import { TransferTon } from "./components/TransferTon";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
-import { useTonConnect } from "./hooks/useTonConnect";
-import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
-import { BrowserRouter, createBrowserRouter, Routes, Route, createRoutesFromElements } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -30,23 +25,11 @@ const AppContainer = styled.div`
 `;
 
 function App() {
-  const { network } = useTonConnect();
-
   return (
     <div className="App">
       <StyledApp>
         <AppContainer>
           <FlexBoxCol>
-            <FlexBoxRow>
-              <TonConnectButton />
-              <Button>
-                {network
-                  ? network === CHAIN.MAINNET
-                    ? "mainnet"
-                    : "testnet"
-                  : "N/A"}
-              </Button>
-            </FlexBoxRow>
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Home />} />
